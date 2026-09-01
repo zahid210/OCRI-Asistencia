8a529b8b3531e335cc9688e7bd5088968e53a618
zahid210 <e_2022200420G@uncp.edu.pe>
2026-09-01 09:48:46 -0500

    allowedHosts de Ngrok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5173,
+    allowedHosts: ['ty-bardish-silvia.ngrok-free.dev'],
     proxy: {
       '/api': 'http://localhost:3000'
     }
